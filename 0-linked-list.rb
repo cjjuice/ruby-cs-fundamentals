3bcc81bd202c43717398aba50b45d546665843fe
C.J. Bordeleau <cj@cj.gg>
2016-10-03 11:26:14 -0400

    Add double linked list.

--- 0-linked-list.rb
+++ 0-linked-list.rb
@@ -8,6 +8,18 @@ class Node
   end
 end
 
+# Double linked
+class DoubleNode
+  attr_accessor :val, :next, :prev
+
+  def initialize(val=nil, next_node=nil, prev_node=nil)
+    @val = val
+    @next = next_node
+    @prev = prev_node
+  end
+end
+
+# make a singly linked list
 root = Node.new(0)
 active = root
 5.times do |n|
@@ -16,3 +28,13 @@ active = root
 end
 
 puts root.inspect
+
+# make a double!
+dbl_root = DoubleNode.new(0)
+dbl_active = dbl_root
+5.times do |n|
+  dbl_active.next = DoubleNode.new(n+1, nil, dbl_active)
+  dbl_active = dbl_active.next
+end
+
+puts dbl_root.inspect
